4c76483c75a4ed8a239c588a25f235f4e927ee9a
Sakeena Syed Naqvi <syedsakeena.2001@gmail.com>
2025-11-18 00:21:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,6 @@
-// Livre qui tombe
-anime({
-    targets: '#livre',
-    top: 300,
-    duration: 2000,
-    easing: 'easeOutBounce',
-    complete: () => {
-        // Page qui tourne
-        anime({
-            targets: '#page1',
-            opacity: 1,
-            rotateY: ['90deg', '0deg'],
-            duration: 1500,
-            easing: 'easeOutExpo',
-            complete: () => {
-                anime({ targets: '#particles', opacity: [0,1], duration: 800, easing: 'easeInOutSine'});
-                createParticles();
-            }
-        });
-    }
+const book = document.querySelector('.book');
+
+book.addEventListener('click', () => {
+    // On ajoute une classe "active" qui va lancer l'animation CSS
+    book.classList.toggle('open');
 });
-
-// Particules lumineuses
-function createParticles() {
-    const container = document.getElementById("particles");
-    for (let i = 0; i < 25; i++) {  
-        const p = document.createElement("div");
-        p.classList.add("particle");
-        p.style.left = "100px";
-        p.style.top = "100px";
-        container.appendChild(p);
-
-        const randomX = (Math.random() - 0.5) * 180;
-        const randomY = -Math.random() * 120;
-
-        anime({
-            targets: p,
-            translateX: randomX,
-            translateY: randomY,
-            opacity: [0,1,0],
-            scale: [0.4,1.3],
-            duration: 1500 + Math.random()*800,
-            easing: 'easeOutQuad',
-            delay: i*100,
-            complete: () => p.remove()
-        });
-    }
-}
