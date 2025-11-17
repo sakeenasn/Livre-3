62460e76e2ada4a0af604ac444df145cd89d9001
Sakeena Syed Naqvi <syedsakeena.2001@gmail.com>
2025-11-18 00:47:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const book = document.getElementById('book');
 
     book.addEventListener('click', () => {
-        // Ajoute ou enlève la classe 'open' au livre
         book.classList.toggle('open');
     });
 });
